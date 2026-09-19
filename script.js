32126651bba4222a1cb441cf2772542477c72af0
Jashnavi1429 <yannamanisrijashnavi@gmail.com>
2026-09-19 15:38:03 +0530

    Add Women Safety Resource Hub website

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    alert("Use the navigation links to explore SafeHer.");
+}
+
+function emergencyHelp() {
+    alert("Emergency Contacts\n\nPolice: 100\nWomen Helpline: 181");
+}
